01d40c3d5605e098a9100b92904641c917bdc493
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2022-11-01 14:57:24 +0100

    Changed profile image

--- profile/src/Profile.js
+++ profile/src/Profile.js
@@ -6,17 +6,17 @@ export default function Profile(){
         <section className="profile">
             <section className="img-wrapper">
                 <img id="profile__img" src="./images/profile__img.png" alt="profile image"/>
-                <h3 id= "twitter">Annette Black</h3>
-                <h3 id="slack">Slack username</h3>
+                <h3 id= "twitter">Joseph-busayo</h3>
+                <h3 id="slack">Joseph-busayo</h3>
             </section>
 
             <section className="links">
                 <div className="link-wrapper">
-                    <a href="" className="link">Twitter</a>
+                    <a href="www.twitter.com/josephbusayo9" className="link">Twitter</a>
                     <a href="https://training.zuri.team/" className="link">Zuri Team</a>
                     <a href="http://books.zuri.team/" className="link">Zuri Books</a>
 
-                    <a href="https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>" className="link">Python Books</a>
+                    <a href="https://books.zuri.team/python-for-beginners?ref_id=Joseph-busayo" className="link">Python Books</a>
                     <a href="python-for-beginners?ref_id=<yourslackname>" className="link">Background Check for Coders</a>
                     <a href="python-for-beginners?ref_id=<yourslackname>" className="link">Design Books</a>
                 </div>
